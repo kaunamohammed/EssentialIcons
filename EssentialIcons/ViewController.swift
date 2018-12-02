4d63b9a976e4aa61e7d5c7596d5f4aabf4deb191
Kauna Mohammed <kaunamohammed@gmail.com>
2018-12-02 18:21:55 +0000

    Initial Commit

---
+++ EssentialIcons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EssentialIcons
+//
+//  Created by Kauna Mohammed on 02/12/2018.
+//  Copyright © 2018 Kauna Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
